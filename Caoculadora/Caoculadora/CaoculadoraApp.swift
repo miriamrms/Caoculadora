21c0585a95ef3726bcc8539f8f2bc2a3f91c156e
Miriam Rayane Mendes da Silva <miriammendes@MacBook-Pro-de-Miriam.local>
2024-05-07 11:20:20 -0300

    adicionei o arquivo do xcode

---
+++ Caoculadora/Caoculadora/CaoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraApp.swift
+//  Caoculadora
+//
+//  Created by Miriam Rayane Mendes da Silva on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
