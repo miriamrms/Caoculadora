ce86502898091a2a7f1e2f07fc548295d6b654ff
Miriam Rayane Mendes da Silva <miriammendes@10-48-48-218.0200.ufpe.br.iosda.org>
2024-05-09 11:50:47 -0300

    add image and button

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State var years: Int? = nil
     @State var months: Int? = nil
+    @State var result: Int?
     
     var body: some View {
         VStack(alignment: .leading){
@@ -20,10 +21,41 @@ struct ContentView: View {
                       format: .number)
           
             Text("Meses")
-            TextField("Quantos meses completos tem seu cão?",
+            TextField("E quantos meses além disso ele tem?",
                       value: $months,
                       format: .number)
             Text("Porte")
+            
+            //botao sem trailing closure
+            //Button(action: {print("Botão foi clicado")}, label: {Text("Clique aqui")})
+            //botao com trailing closure
+//            Button("Clique aqui"){
+//                print("Botão foi clicado")
+//            }
+//            .frame(width: 300)
+//            .padding()
+//            .background(Color.blue)
+//            .foregroundColor(.white)
+//            .cornerRadius(10)
+            if let result {
+                Text("Seu cachorro tem, em idade humana...")
+                Text("\(result) anos")
+            } else {
+                Image(ImageResource.clarinha)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxHeight: 150)
+                    .frame(maxWidth: .infinity)
+            }
+            Button("Cãocular"){
+                print("cãocular")
+                result = 23
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 50)
+            .background(.indigo)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
         }
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
