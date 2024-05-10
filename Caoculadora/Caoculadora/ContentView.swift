132446dedc92c8e73a95f701534ba13ccc4206c4
Miriam Rayane Mendes da Silva <miriammendes@10-48-48-218.0200.ufpe.br.iosda.org>
2024-05-10 11:34:24 -0300

    add segment control

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     @State var years: Int? = nil
     @State var months: Int? = nil
     @State var result: Int?
+    @State var porteSelecionado = "Pequeno"
+    let portes = ["Pequeno", "Medio", "Grande"]
     
     var body: some View {
         VStack(alignment: .leading){
@@ -26,6 +28,15 @@ struct ContentView: View {
                       format: .number)
             Text("Porte")
             
+            Picker("Portes", selection: $porteSelecionado){
+                ForEach(portes, id: \.self){
+                    porte in
+                    Text(porte)
+                }
+            }
+            .pickerStyle(.segmented)
+            .padding()
+            
             //botao sem trailing closure
             //Button(action: {print("Botão foi clicado")}, label: {Text("Clique aqui")})
             //botao com trailing closure
@@ -37,6 +48,7 @@ struct ContentView: View {
 //            .background(Color.blue)
 //            .foregroundColor(.white)
 //            .cornerRadius(10)
+            
             if let result {
                 Text("Seu cachorro tem, em idade humana...")
                 Text("\(result) anos")
@@ -46,20 +58,46 @@ struct ContentView: View {
                     .scaledToFit()
                     .frame(maxHeight: 150)
                     .frame(maxWidth: .infinity)
+                    .shadow(radius: 5)
             }
-            Button("Cãocular"){
-                print("cãocular")
-                result = 23
-            }
+            
+            Spacer()
+
+            Button("Cãocular", action: processYears)
             .frame(maxWidth: .infinity)
             .frame(height: 50)
             .background(.indigo)
             .foregroundColor(.white)
             .clipShape(RoundedRectangle(cornerRadius: 10))
+            .bold()
         }
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
         .padding()
+    }
+    
+    func processYears(){
+        print("cãocular")
+        guard let years else {
+            print("Campos não preenchidos")
+            return }
+        guard let months else { return }
+        guard months > 0 || years > 0 else{
+            return
+        }
+        let  multiplicador: Int
+        switch porteSelecionado {
+        case "Pequeno":
+            multiplicador = 6
+        case "Medio":
+            multiplicador = 7
+        case "Grande":
+            multiplicador = 8
+        default:
+            multiplicador = 0
+        }
+        result = years * multiplicador + ((months*multiplicador)/12)
+        
     }
 }
 
